a00376f7591f21ef8c54b151822e779749c44e18
md <hasnainshaikh62479@gmail.com>
2024-02-01 08:38:11 +0530

    first backend

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+require('dotenv').config()
 const express = require('express')
 const app = express()
 const port = 3000
@@ -5,10 +6,11 @@ const port = 3000
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
+
 app.get('/home', (req, res) => {
     res.send("Its my Home page");
 })
 
-app.listen(port, () => {
+app.listen(process.env.PORT, () => {
   console.log(`Example app listening on port ${port}`)
 })
